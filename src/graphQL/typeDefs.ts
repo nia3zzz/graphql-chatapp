9c0afc1a61cb240c98040fe45fe10a923ebadd27
nia3zzz <mushfiqurniaz33@gmail.com>
2025-12-08 02:58:33 +0600

    add graphql type def

--- src/graphQL/typeDefs.ts
+++ src/graphQL/typeDefs.ts
@@ -1,4 +1,35 @@
 const typeDefs: string = `
+      scalar Date
+
+      type User {
+        id:ID!
+        name:String!
+        username:String!
+        email:String!
+        profilePicture:String!
+        createdAt:Date!
+        updatedAt:Date!
+      }
+
+      type Chat {
+        id:ID!
+        chatName:String!
+        isGroupChat:Boolean!
+        participants:[User!]!
+        groupAdmin:User!
+        createdAt:Date!
+        updatedAt:Date!
+      }
+
+      type Message {
+        id:ID!
+        sender:User!
+        reciever:User!
+        content:String!
+        createdAt:Date!
+        updatedAt:Date!
+      }
+
       type Query {
         hello: String!
       }
